bb07ac09f802e6d20c5d2dfbb2d34b8e008b25e7
Rasmus Wulff Jensen <rasmus.wulff.jensen@outlook.com>
2023-02-05 15:36:08 +0100

    Added missing XML Comments

---
+++ TrelloDotNet/TrelloDotNet/Model/TrelloClientOptions.cs
@@ -0,0 +1,30 @@
+﻿namespace TrelloDotNet.Model
+{
+    /// <summary>
+    /// Options for the Trello Client
+    /// </summary>
+    public class TrelloClientOptions
+    {
+        /// <summary>
+        /// Control level of URL Details are shown in Exceptions from calls to the API
+        /// </summary>
+        public ApiCallExceptionOption ApiCallExceptionOption { get; set; }
+        /// <summary>
+        /// Controls if it is allowed to delete Boards (secondary confirmation)
+        /// </summary>
+        public bool AllowDeleteOfBoards { get; set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="apiCallExceptionOption">Control level of URL Details are shown in Exceptions from calls to the API</param>
+        /// <param name="allowDeleteOfBoards">Controls if it is allowed to delete Boards (secondary confirmation)</param>
+        public TrelloClientOptions(
+            ApiCallExceptionOption apiCallExceptionOption = ApiCallExceptionOption.IncludeUrlButMaskCredentials,
+            bool allowDeleteOfBoards = false)
+        {
+            ApiCallExceptionOption = apiCallExceptionOption;
+            AllowDeleteOfBoards = allowDeleteOfBoards;
+        }
+    }
+}
--- TrelloDotNet/TrelloDotNet/TrelloClientOptions.cs
+++ TrelloDotNet/TrelloDotNet/TrelloClientOptions.cs
@@ -3,32 +3,21 @@
 namespace TrelloDotNet
 {
     /// <summary>
-    /// Options for the Trello Client
+    /// Control level of URL Details are shown in Exceptions from calls to the API
     /// </summary>
-    public class TrelloClientOptions
-    {
-        public ApiCallExceptionOption ApiCallExceptionOption { get; set; }
-        public bool AllowDeleteOfBoards { get; set; }
-
-        public TrelloClientOptions(
-            ApiCallExceptionOption apiCallExceptionOption = ApiCallExceptionOption.IncludeUrlButMaskCredentials,
-            bool allowDeleteOfBoards = false)
-        {
-            ApiCallExceptionOption = apiCallExceptionOption;
-            AllowDeleteOfBoards = allowDeleteOfBoards;
-        }
-    }
-
-    public enum DeleteMethodsUsage
-    {
-        Enabled,
-        Disabled
-    }
-
     public enum ApiCallExceptionOption
     {
+        /// <summary>
+        /// Exception show entire URL + Credentials
+        /// </summary>
         IncludeUrlAndCredentials,
+        /// <summary>
+        /// Exception show entire URL but credentials are replaced with XXXXX
+        /// </summary>
         IncludeUrlButMaskCredentials,
+        /// <summary>
+        /// The URL that caused the exception are not shown
+        /// </summary>
         DoNotIncludeTheUrl
     }
 }
