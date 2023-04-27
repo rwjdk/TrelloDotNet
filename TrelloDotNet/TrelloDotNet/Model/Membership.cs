a3f07f3c9ac817d9b3c8eaf9bf55096d28ea215c
Rasmus Wulff Jensen <rasmus.wulff.jensen@gmail.com>
2023-04-27 20:18:37 +0200

    - Bump to v 1.5.0 (#11)
    - Added 'GetMembershipsOfBoardAsync'
    - Added Automation Action: 'RemoveLabelsFromCardAction'
    - It is now legal to have a null as Condition in Automation Engine (indicating there are no further conditions)

---
+++ TrelloDotNet/TrelloDotNet/Model/Membership.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.Json.Serialization;
+using TrelloDotNet.Control;
+
+namespace TrelloDotNet.Model
+{
+    /// <summary>
+    /// Represent a Membership on a Board
+    /// </summary>
+    public class Membership
+    {
+        /// <summary>
+        /// Id of the Membership
+        /// </summary>
+        [JsonPropertyName("id")]
+        [JsonInclude]
+        public string Id { get; private set; }
+
+        /// <summary>
+        /// Id of the Member this Membership-entry represent
+        /// </summary>
+        [JsonPropertyName("idMember")]
+        [JsonInclude]
+        public string MemberId { get; private set; }
+
+        /// <summary>
+        /// The Type of the Members (admin, normal, observer)
+        /// </summary>
+        [JsonPropertyName("memberType")]
+        [JsonInclude]
+        [JsonConverter(typeof(EnumViaJsonPropertyConverter<MembershipType>))]
+        public MembershipType MemberType { get; private set; }
+
+        /// <summary>
+        /// If the member is unconfirmed
+        /// </summary>
+        [JsonPropertyName("unconfirmed")]
+        [JsonInclude]
+        public bool Unconfirmed { get; private set; }
+
+        /// <summary>
+        /// If the member is deactivated
+        /// </summary>
+        [JsonPropertyName("deactivated")]
+        [JsonInclude]
+        public bool Deactivated { get; private set; }
+    }
+}
---
+++ TrelloDotNet/TrelloDotNet/Model/MembershipType.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TrelloDotNet.Model
+{
+    public enum MembershipType
+    {
+        [JsonPropertyName("admin")]
+        Admin,
+        [JsonPropertyName("normal")]
+        Normal,
+        [JsonPropertyName("observer")]
+        Observer
+    }
+}
